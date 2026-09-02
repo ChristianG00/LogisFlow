95bafc0620d675dacbefb54bb9813003e699541b
Christian Gallardo <Christiangallardo.gb@gmail.com>
2026-09-01 21:34:38 -0400

    Configuracion final: variables de entorno seguras y requirements actualizados

--- LogisFlow/urls.py
+++ LogisFlow/urls.py
@@ -1,13 +1,19 @@
-from django.contrib import admin
+import os
+
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
 ]
 
-# Esto permite que Django muestre las fotos subidas
+# Fotos públicas de productos en AlwaysData
+urlpatterns += static(
+    f'{settings.MEDIA_URL}productos/',
+    document_root=os.path.join(settings.MEDIA_ROOT, 'productos'),
+)
+
+# Archivos multimedia adicionales durante el desarrollo local
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
